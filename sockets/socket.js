12d2b42a6f4df8194b29c8ecc152ef5381255f3c
Erick Cavero Levano <ecaverolevano@gmail.com>
2021-01-18 21:24:31 -0500

    Cambios sobre bandas terminado

--- sockets/socket.js
+++ sockets/socket.js
@@ -1,19 +1,62 @@
 const { io } = require('../index');
+const Band = require('../models/band');
+const Bands  = require('../models/bands');
+
+const bands = new Bands();
+
+bands.addBand( new Band('Queen'));
+bands.addBand( new Band('Bon Jovi'));
+bands.addBand( new Band('Heroes del Silencio'));
+bands.addBand( new Band('Metallica'));
+
+// console.log(bands);
+
 
 //Mensajes de Sockets
 io.on('connection', client => {
 
     console.log('Cliente conectado');
 
+    client.emit('active-bands', bands.getBands());
+
     client.on('disconnect', () =>{
         console.log('Cliente desconectado');
     });
 
-    //escuchar evento
-    client.on('mensaje', (payload) => {
-        console.log('Mensaje', payload);
+    //Emitir mensaje
+    // client.on('emitir-mensaje', ( payload ) => {
 
-        //emitir evento
-        io.emit('mensaje', {admin: 'Nuevo mensaje'});
-    })
+    //     //Se emite a todos
+    //     // io.emit('nuevo-mensaje', payload);
+
+    //     //Se emite a todos menos al que emitió
+    //     client.broadcast.emit('nuevo-mensaje', payload);
+
+    // });
+
+    client.on('vote-band', (payload) => {
+
+        bands.voteBand( payload.id );
+
+        io.emit('active-bands', bands.getBands());
+
+    });
+
+    client.on('add-band', (payload) =>{
+
+        // console.log(payload);
+
+        var band = new Band(payload.name);
+
+        bands.addBand(band);
+
+        io.emit('active-bands', bands.getBands());
+
+
+    });
+
+    client.on('delete-band', (payload) =>{
+        bands.deleteBand(payload.id);
+        io.emit('active-bands', bands.getBands());
+    });
 });
